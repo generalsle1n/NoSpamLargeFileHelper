51cbdb4086b9c6e4f66060c4d38e206e3c447cb5
Niels Schuler <50528824+generalsle1n@users.noreply.github.com>
2023-05-31 09:51:13 +0200

    Added Method for virustotal api

--- NospamHelper/VirustotalHelper.cs
+++ NospamHelper/VirustotalHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NospamHelper.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,56 +12,107 @@ namespace NospamHelper
 {
     public class VirustotalHelper
     {
-        private const string _api = "86cab9f34bcd9542f07e7ec3b66a9933ceea130074b5e4c271de1df31a19c0ed";
-        VirusTotal _virusTotal = new VirusTotal(_api);
+        VirusTotal _virusTotal;
         private readonly ILogger<VirustotalHelper> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly NoSpamHelper _nospamHelper;
+        private const int _waitTime = 60 * 1000;
+        private const int _loopMaxUpload = 5;
 
-        public VirustotalHelper(ILogger<VirustotalHelper> logger)
+        public VirustotalHelper(ILogger<VirustotalHelper> logger, IConfiguration configuration, NoSpamHelper NospamHelper)
         { 
             _logger = logger; 
+            _configuration = configuration;
+            _nospamHelper = NospamHelper;
+
+            InitApi();
         }
 
+        private void InitApi()
+        {
+            _virusTotal = new VirusTotal(_configuration.GetValue<string>("VirusTotalApi"));
+            _virusTotal.UseTLS = true;
+            //_virusTotal.Timeout = new TimeSpan(0,5,0);
+        }
 
-        internal async Task<ScanResult> UploadFile(byte[] Data, string FileName)
+        internal async Task<ResultFromScan> UploadFile(byte[] Data, string FileName)
         {
             ScanResult Result = await _virusTotal.ScanFileAsync(Data, FileName);
-            _logger.LogInformation($"Upload File {FileName}");
-            return Result;
-        }
-
-        internal async Task<FileReport> CheckIfFileIsClean(byte[] Data)
-        {
-            FileReport Result = await _virusTotal.GetFileReportAsync(Data);
-            _logger.LogInformation($"Try to get File Report");
-            return Result;
-        }
-
-        internal async Task<bool> ProccessFile(byte[] Data, string FileName)
-        {
-            FileReport report = await CheckIfFileIsClean(Data);
-
-            bool Result = false;
-
-            if((report.ResponseCode == FileReportResponseCode.Present) && report.Positives == 0)
+            _logger.LogInformation($"Uploaded File {FileName}");
+            int count = 0;
+            while(count <= _loopMaxUpload)
             {
-                Result = true;
-            }else if(report.ResponseCode == FileReportResponseCode.NotPresent)
-            {
-                await UploadFile(Data, FileName);
-                int loopcont = 0;
-                int timecount = 20;
-                while(loopcont <= 5)
+                FileReport report = await GetFileReport(Data);
+                ResultFromScan result = DecideOperationOnFileReport(report);
+
+                if(result == ResultFromScan.Clean)
                 {
-                    FileReport reportScanned = await CheckIfFileIsClean(Data);
-                    if((report.ResponseCode == FileReportResponseCode.Present) && report.Positives == 0){
-                        Result = true;
-                    }
-                    loopcont++;
-                    await Task.Delay(timecount * 1000);
-                    timecount += timecount;
+                    return result;
+                }
+                else
+                {
+                    _logger.LogInformation($"Wait for Result {_waitTime}");
+                    await Task.Delay(_waitTime);
                 }
             }
+            return ResultFromScan.NotClean;
+        }
+
+        internal async Task<FileReport> GetFileReport(byte[] Data)
+        {
+            FileReport Result = await _virusTotal.GetFileReportAsync(Data);
             return Result;
         }
+
+
+        private ResultFromScan DecideOperationOnFileReport(FileReport Result)
+        {
+            if (Result.ResponseCode == FileReportResponseCode.Present && Result.Positives == 0)
+            {
+                _logger.LogInformation($"{Result.SHA256} is Clean");
+                return ResultFromScan.Clean;
+            }
+            else if(Result.ResponseCode == FileReportResponseCode.NotPresent || Result.ResponseCode == FileReportResponseCode.Queued)
+            {
+                _logger.LogInformation($"{Result.Resource} needs to be scanned");
+                return ResultFromScan.NotScanned;
+            }else
+            {
+                _logger.LogWarning($"{Result.SHA256} is not Clean");
+                return ResultFromScan.NotClean;
+            }
+        }
+
+        internal async Task<bool> ProccessFile(byte[] Data, LargeFileEntry File)
+        {
+            FileReport fileReport = await GetFileReport(Data);
+            ResultFromScan res = DecideOperationOnFileReport(fileReport);
+            if(res == ResultFromScan.Clean)
+            {
+                return true;
+            }
+            else if(res == ResultFromScan.NotScanned)
+            {
+                if (await UploadFile(Data, File.Name) == ResultFromScan.Clean)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    enum ResultFromScan
+    {
+        Clean,
+        NotClean,
+        NotScanned
     }
 }
