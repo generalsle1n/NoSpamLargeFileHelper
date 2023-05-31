61f5a4ba5c2a9b7068505cfe4707207b4b0cfbc7
Niels Schuler <50528824+generalsle1n@users.noreply.github.com>
2023-05-31 09:48:33 +0200

    Added Job Config

--- NospamHelper/Program.cs
+++ NospamHelper/Program.cs
@@ -19,6 +19,33 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddHostedService<Worker>();
         services.AddSingleton<NoSpamHelper>();
         services.AddSingleton<VirustotalHelper>();
+
+        services.AddQuartzHostedService(settings =>
+        {
+            settings.WaitForJobsToComplete = true;
+        });
+
+        services.AddQuartz(queue =>
+        {
+            queue.UseMicrosoftDependencyInjectionJobFactory();
+            queue.AddJob<Worker>(job =>
+            {
+                job.WithIdentity(MainJob);
+            });
+
+            queue.AddTrigger(trigger =>
+            {
+                trigger.StartNow();
+                trigger.WithSimpleSchedule(schedule =>
+                {
+                    schedule.WithIntervalInMinutes(tempConfig.GetValue<int>("IntervalInMinutes"));
+                    schedule.RepeatForever();
+                });
+                trigger.ForJob(MainJob);
+            });
+        });
+    })
+    })
     })
     .Build();
 
