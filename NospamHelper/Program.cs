299f2d022032379446164d77e8e3735a9f88b8b4
Niels Schuler <50528824+generalsle1n@users.noreply.github.com>
2023-05-31 09:48:07 +0200

    Added Job for quartz and setup config

--- NospamHelper/Program.cs
+++ NospamHelper/Program.cs
@@ -1,4 +1,17 @@
 using NospamHelper;
+using Quartz;
+using Serilog;
+using System.Reflection;
+
+JobKey MainJob = new JobKey("MainJob");
+
+string binaryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+string logPathFolder = Path.Combine(binaryPath, "Logs","VirusTotal.log");
+string configPath = Path.Combine(binaryPath, "appsettings.json");
+
+IConfiguration tempConfig = new ConfigurationBuilder()
+    .AddJsonFile(configPath)
+    .Build();
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
